a162b75bbdf449d5a43853080154e658711d43ea
Mohammed Al-Hafidh <66840344+Mohammed-Al-Hafidh@users.noreply.github.com>
2020-12-13 12:41:56 -0500

    Adding the files of the project

---
+++ Skaters/Skaters/Skaters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Skaters
+{
+    public class Skaters
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public double[] TechnicalAspects { get; set; }
+        public double[] PerformanceAspects { get; set; }
+        public double FinalScore { get; set; }
+        public string Medal { get; set; }
+        public Skaters(string FirstName, string LastName, string Country, double[] TechnicalAspects, double[] PerformanceAspects)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Country = Country;
+            this.TechnicalAspects = TechnicalAspects;
+            this.PerformanceAspects = PerformanceAspects;
+            this.Medal = "NONE";
+            calcFinalScore();
+        }
+
+        private void calcFinalScore()
+        {
+            double techSum = 0;
+            int counter = 0;
+            foreach (double score in TechnicalAspects)
+            {
+                techSum += score;
+                counter++;
+            }
+            techSum = techSum / counter;
+
+            double perSum = 0;
+            counter = 0;
+            foreach (double score in PerformanceAspects)
+            {
+                perSum += score;
+                counter++;
+            }
+            perSum = perSum / counter;
+            FinalScore = techSum + perSum;
+            FinalScore = Math.Round(FinalScore, 3);
+        }
+
+
+        public override string ToString()
+        {
+            string format = "{0,-20} {1,-20} {2,-20} {3,-35} {4,-35} {5,-20} {6,-20}";
+            string result = string.Format(format,
+                 FirstName, LastName, Country, string.Join(" ", TechnicalAspects), string.Join(" ", PerformanceAspects), FinalScore, Medal);
+            return result;
+        }
+
+
+    }
+}
---
+++ Skaters/Skaters/SkatersDemo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Skaters
+{
+    class SkatersDemo
+    {
+        private static List<Skaters> skatersArray;
+        private static int counter;
+        static void Main(string[] args)
+        {
+            counter = 0;
+            skatersArray = new List<Skaters>();
+            readFile();
+            //Print the file before sorting
+            Console.WriteLine("The file before sorting:\n");
+            string format = "{0,-20} {1,-20} {2,-20} {3,-35} {4,-35} {5,-20} {6,-20}";
+            string result = string.Format(format, "First Name", "Last Name", "Country", "Technical Aspects",
+                "Performance Aspects", "Final Score", "Medal");
+            string underLine = string.Format(format, "----------", "---------", "-------", "-----------------",
+                "-------------------", "-----------", "-----");
+            Console.WriteLine(result);
+            Console.WriteLine(underLine);
+            foreach (Skaters skaters in skatersArray)
+            {
+                Console.WriteLine(skaters.ToString());
+            }
+            List<Skaters> skatersArraySorted = skatersArray.OrderByDescending(o => o.FinalScore).ToList();
+            skatersArraySorted[0].Medal = "Gold";
+            skatersArraySorted[1].Medal = "Silver";
+            skatersArraySorted[2].Medal = "Bronze";
+            //Print the final result after sorting
+            Console.WriteLine("\n\nThe final result after sorting:\n");
+            Console.WriteLine(result);
+            Console.WriteLine(underLine);
+            foreach (Skaters skaters in skatersArraySorted)
+            {
+                Console.WriteLine(skaters.ToString());
+            }
+        }
+        public static void readFile()
+        {
+            string tempFirstName, tempLastName, tempCountry, temp1, temp2;
+            string[] tempTechAsp;
+            string[] tempPerAsp;
+            double[] tempTechnicalAspects = null;
+            double[] tempPerformanceAspects = null;
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\mhmdm\\Desktop\\Google Drive\\Study in Canada\\JOHNABBOTT\\The study\\.NET PROGRAMMING FUNDAMENTALS\\Final Project\\Pairs.txt");
+
+                //Read the first line of text
+                string line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    tempFirstName = line;
+                    tempLastName = sr.ReadLine();
+                    tempCountry = sr.ReadLine();
+                    temp1 = sr.ReadLine();
+                    tempTechAsp = temp1.Split(' ');
+                    temp2 = sr.ReadLine();
+                    tempPerAsp = temp2.Split(' ');
+                    tempTechnicalAspects = new double[tempTechAsp.Length];
+                    tempPerformanceAspects = new double[tempPerAsp.Length];
+                    for (int i = 0; i < 8; i++)
+                    {
+                        tempTechnicalAspects[i] = double.Parse(tempTechAsp[i]);
+                        tempPerformanceAspects[i] = double.Parse(tempPerAsp[i]);
+                    }
+
+                    Skaters tempSkaters = new Skaters(tempFirstName, tempLastName, tempCountry, tempTechnicalAspects, tempPerformanceAspects);
+                    skatersArray.Add(tempSkaters);
+                    counter++;
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+            }
+        }
+    }
+}
